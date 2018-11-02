96c6333d74acd73e69c5b5d4e19123dc378218dd
Julian Wong <jwong01@stuy.edu>
2018-11-02 11:00:16 -0400

    finish

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -23,4 +23,24 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
 			return true;
 		}
 	}
+	public void add(int ind, T obj) {
+		if(this.size() == 0) {
+			super.add(obj);
+		}
+		else {
+			for(int q = 0; q < this.size(); q++) {
+				if(obj.compareTo(this.get(q)) <= 0) {
+					super.add(q, obj);
+					break;
+				}
+			}
+		}
+	}
+	
+	public T set(int ind, T obj) {
+		T old = super.remove(ind);
+		this.add(obj);
+		return old;
+	}
+	
 }
